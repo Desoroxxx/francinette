87d0f366d8b5573a944c601255b2d9b446048f19
Desoroxxx <desoroxxx.git@redstudio.dev>
2025-10-23 20:15:05 +0200

    Update git_url to point to the correct repository

--- testers/get_next_line/Fsoares.py
+++ testers/get_next_line/Fsoares.py
@@ -17,7 +17,7 @@ class Fsoares(BaseExecutor):
 
 	name = 'fsoares'
 	folder = 'fsoares'
-	git_url = 'my own tests'
+	git_url = 'https://github.com/Desoroxxx/francinette/tree/main/testers/get_next_line'
 	line_regex = re.compile(r"^([^:]+):(.+)$")
 	test_regex = re.compile(r"(\d+)(?:[^ ]+)?\.([^ ]+)")
 
