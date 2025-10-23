23f1a6e79b37421367096caaf35340e5357e9cf5
Desoroxxx <desoroxxx.git@redstudio.dev>
2025-10-23 20:16:21 +0200

    Update git_url to point to the correct repository

--- testers/pipex/Fsoares.py
+++ testers/pipex/Fsoares.py
@@ -91,7 +91,7 @@ class Fsoares(BaseExecutor):
 
 	name = 'fsoares'
 	folder = 'fsoares'
-	git_url = 'my own tests'
+	git_url = 'https://github.com/Desoroxxx/francinette/tree/main/testers/pipex'
 
 	def __init__(self, tests_dir, temp_dir, to_execute, missing) -> None:
 		super().__init__(tests_dir, temp_dir, to_execute, missing)
